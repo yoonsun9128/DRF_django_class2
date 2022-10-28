2dbdf8265464e9eb5d7114d767b0a94f580d8371
yoonsun9128 <yoonsun9128@gmail.com>
2022-10-28 11:19:16 +0900

    articles init

--- DRF_class2/settings.py
+++ DRF_class2/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'users',
+    'articles',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- DRF_class2/urls.py
+++ DRF_class2/urls.py
@@ -5,4 +5,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('users.urls')),
+    path('articles/', include('articles.urls')),
 ]
