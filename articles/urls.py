0501f091cf54d73ac58095319c6bbb33a2707488
yoonsun9128 <yoonsun9128@gmail.com>
2022-10-28 13:33:27 +0900

    pip install

--- articles/urls.py
+++ articles/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', views.ArticlesView.as_view(), name="Articlesview"),
     path('<int:article_id>/', views.ArticleDetailView.as_view(), name="Article_detail"),
     path('comment/', views.CommnetView.as_view(), name="Commentview"),
-    path('comment/<int:commnet_id>', views.CommnetDetailView.as_view(), name="Comment_detail"),
+    path('comment/<int:commnet_id>', views.CommentDetailView.as_view(), name="Comment_detail"),
     path('like/', views.LikeView.as_view(), name="Likeview"),
 ]
