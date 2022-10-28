de9dfe5811b76bb184bd805b45148935c1ffdca5
yoonsun9128 <yoonsun9128@gmail.com>
2022-10-28 13:22:42 +0900

    make article model

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,17 @@
+from turtle import title
+from unittest.util import _MAX_LENGTH
 from django.db import models
+from users.models import User
 
 # Create your models here.
+class Article(models.Model):
+    # 유저 삭제하면 게시물 삭제하게 설정함
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    img = models.ImageField()
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self,title)
