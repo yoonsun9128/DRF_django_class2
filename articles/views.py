c624e29bc11e5fec874385e4f7b4c7c34952cdd7
yoonsun9128 <yoonsun9128@gmail.com>
2022-10-28 11:37:07 +0900

    articles urls logic

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from ast import Delete
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, permissions
 
-# Create your views here.
+# 게시글 보여주기 등록하기
+class ArticlesView(APIView):
+    def get (self, request):
+        pass
+    def post (self, request):
+        pass
+
+# 게시글 상세페이지 및 수정 삭제
+class ArticleDetailView(APIView):
+    def get (self, request, article_id):
+        pass
+    def put (self, request, article_id):
+        pass
+    def delete (self, request, article_id):
+        pass
+
+# 댓글창
+class CommnetView(APIView):
+    def get (self, request, article_id):
+        pass
+    def post (self, request, article_id):
+        pass
+
+# 댓글 수정 삭제
+class CommentDetailView(APIView):
+    def put (self, request, commnet_id):
+        pass
+    def delete (self, request, commnet_id):
+        pass
