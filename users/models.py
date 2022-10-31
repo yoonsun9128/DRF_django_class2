10477ca01dfa1409dfe93bee139c50201d762ef6
yoonsun9128 <yoonsun9128@gmail.com>
2022-10-31 20:10:57 +0900

    profile edit

--- users/models.py
+++ users/models.py
@@ -44,7 +44,7 @@ class User(AbstractBaseUser):
     )
     nickname = models.CharField(max_length=10, null=True)
     # 팔로워 모델 추가
-    followings = models.ManyToManyField('self', symmetrical=False, related_name='followers')
+    followings = models.ManyToManyField('self', symmetrical=False, related_name='followers', blank=True)
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
 
--- users/serializers.py
+++ users/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from users.models import User
+from articles.serializers import ArticleListSerializer
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -25,9 +26,17 @@ class UserSerializer(serializers.ModelSerializer):
         return user
 
 class UserProfileSerializer(serializers.ModelSerializer):
+    # 아이디값으로 보임
+    followers = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    # 이메일 값으로 보임
+    # followers = serializers.StringRelatedField(many=True)
+    # 작성한 글 띄우기
+    article_set = ArticleListSerializer(many=True)
+    # 유저가 좋아요한 글 보이기
+    like_articles = ArticleListSerializer(many=True)
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ("id", "email", "followings", "followers", "article_set", "like_articles")
 
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
